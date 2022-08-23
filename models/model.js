a8d3dfd6e02d65879c7339b1cb95f8fb9953278c
Kamzin <olegkamzin@gmail.com>
2022-08-23 18:20:23 +0300

    Страны моделей

--- models/model.js
+++ models/model.js
@@ -4,7 +4,8 @@ const schema = new db.Schema({
 	name: { type: String, required: true, maxlength: 255, minlength: 2, trim: true, index: true, unique: true },
 	description: { type: String, maxlength: 2000, trim: true },
 	brand: { type: db.Types.ObjectId, ref: 'brands', required: true },
-    rating: { type: Number, max: 5, min: 0 },
+	rating: { type: Number, max: 5, min: 0 },
+	country: { type: String },
 	slug: { type: String, slug: 'name', unique: true },
 	img: { type: Array, required: true }
 })
